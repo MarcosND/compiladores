0a8ea472730f83ecebddb64031c85abe12bcb133
Marcos André <61971624+MarcosND@users.noreply.github.com>
2022-10-27 19:46:50 -0300

    Update RPN.java

--- RPN.java
+++ RPN.java
@@ -1,5 +1,5 @@
 public class RPN {
-    // Devido a entrega da última task esse arquivo foi deletador, por favor ir para
+    // Devido a entrega da última task esse arquivo foi deletado, por favor ir para
     // a raiz do repo: https://github.com/MarcosND/compiladores/
     // Para ver atividades antigas basta ver os outros commits
 }
